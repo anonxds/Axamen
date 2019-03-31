fa6d385bf435ff28f938c51c3c566f29829e6365
Orlando <orlandosanch3z@gmail.com>
2019-03-30 18:28:16 -0700

    cloid

--- Axamen/Form1.Designer.cs
+++ Axamen/Form1.Designer.cs
@@ -34,7 +34,9 @@
             this.txtcontrasena = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.btningresar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btningresar
@@ -42,9 +44,10 @@
             this.btningresar.BackColor = System.Drawing.Color.SeaGreen;
             this.btningresar.Image = ((System.Drawing.Image)(resources.GetObject("btningresar.Image")));
             this.btningresar.ImageActive = null;
-            this.btningresar.Location = new System.Drawing.Point(149, 122);
+            this.btningresar.Location = new System.Drawing.Point(99, 79);
+            this.btningresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btningresar.Name = "btningresar";
-            this.btningresar.Size = new System.Drawing.Size(71, 71);
+            this.btningresar.Size = new System.Drawing.Size(47, 46);
             this.btningresar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btningresar.TabIndex = 0;
             this.btningresar.TabStop = false;
@@ -53,52 +56,68 @@
             // 
             // txtusario
             // 
-            this.txtusario.Location = new System.Drawing.Point(138, 45);
+            this.txtusario.Location = new System.Drawing.Point(92, 29);
+            this.txtusario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtusario.Name = "txtusario";
-            this.txtusario.Size = new System.Drawing.Size(100, 26);
+            this.txtusario.Size = new System.Drawing.Size(68, 20);
             this.txtusario.TabIndex = 1;
             // 
             // txtcontrasena
             // 
-            this.txtcontrasena.Location = new System.Drawing.Point(138, 90);
+            this.txtcontrasena.Location = new System.Drawing.Point(92, 58);
+            this.txtcontrasena.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtcontrasena.Name = "txtcontrasena";
             this.txtcontrasena.PasswordChar = '*';
-            this.txtcontrasena.Size = new System.Drawing.Size(100, 26);
+            this.txtcontrasena.Size = new System.Drawing.Size(68, 20);
             this.txtcontrasena.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 48);
+            this.label1.Location = new System.Drawing.Point(45, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(40, 93);
+            this.label2.Location = new System.Drawing.Point(27, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Contrasena";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 238);
+            this.ClientSize = new System.Drawing.Size(471, 383);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtcontrasena);
             this.Controls.Add(this.txtusario);
             this.Controls.Add(this.btningresar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.btningresar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +130,7 @@
         private System.Windows.Forms.TextBox txtcontrasena;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Axamen/Form1.cs
+++ Axamen/Form1.cs
@@ -8,13 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data.MySqlClient;
+
 namespace Axamen
 {
     public partial class Form1 : Form
     {
+        string connectionString = "Server=traficdb.mysql.database.azure.com; Port=3306; Database=Parras; Uid=orlando@traficdb; Pwd=Bejeweled2012; SslMode=Preferred;";
+     //   BaseDeDatos db = new BaseDeDatos();
         public Form1()
         {
             InitializeComponent();
+            //     dataGridView1.DataSource = db.SelectDataTable("select * from lideres");
+            fill();
+        }
+        private void fill()
+        {
+            MySqlConnection con = new MySqlConnection(connectionString);
+            MySqlDataAdapter ad = new MySqlDataAdapter("select * from lideres",con);
+            con.Open();
+            DataSet ds = new DataSet();
+            ad.Fill(ds, "id");
+            dataGridView1.DataSource = ds.Tables["id"];
+            con.Close();
+
         }
 
         private void btningresar_Click(object sender, EventArgs e)
@@ -22,6 +39,11 @@ namespace Axamen
             Menu m = new Menu();
             m.Show();
             this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
